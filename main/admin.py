bbe5e150bb24d2c66cf190f1d9e4b2cd9f491e8e
oleg <olegpustovalov220@gmail.com>
2022-10-10 11:39:17 +0300

    add new models from top

--- main/admin.py
+++ main/admin.py
@@ -57,10 +57,7 @@ class CatalogServiceAdmin(NonSortableParentAdmin):
 @admin.register(MidServices)
 class MidServicesAdmin(admin.ModelAdmin):
     """Раздел середина"""
-    list_display = 'title', 'position', 'is_active'
-    list_editable = 'position', 'is_active'
-    list_filter = 'is_active',
-    search_fields = 'name',
+    pass
 
     # def has_module_permission(self, request):
     #     return False
@@ -81,29 +78,7 @@ class MidImageInline(SortableStackedInline):
 @admin.register(MidCatalogService)
 class MidCatalogServiceAdmin(NonSortableParentAdmin):
     """Услуги середина"""
-    # list_display = 'name', 'marker', 'position', 'service', 'is_active',
-    # list_editable = 'marker', 'is_active', 'position', 'service', 'is_active',
-    # list_filter = 'marker', 'service', 'is_active',
-    # search_fields = 'name',
+    pass
     inlines = [MidImageInline]
 
-# @admin.register(CatalogService)
-# class CatalogServiceAdmin(admin.ModelAdmin):
-#     """Услуги"""
-#     list_display = 'name', 'marker', 'position', 'service', 'is_active',
-#     list_editable = 'marker', 'is_active', 'position', 'service', 'is_active',
-#     list_filter = 'marker', 'service', 'is_active',
-#     search_fields = 'name',
-#     inlines = ImageInline,
 
-
-# class ImageInline(admin.TabularInline):
-#     """Фотографии"""
-#     model = Image
-#     extra = 2
-#     readonly_fields = 'preview',
-#
-#     def preview(self, obj):
-#         return mark_safe(f'<img src="{obj.image.url}" width="80" height="80">')
-#
-#     preview.short_description = 'Превью Фотографии'
