139828b680555c41f4c3472dae8253c079752f4d
oleg <olegpustovalov220@gmail.com>
2022-10-10 13:45:22 +0300

    new admin.py

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
-from main.models import FotoSliderBase, TimeSlideBase, Service, CatalogService, Image, Position, \
-    MidServices, MidImage, MidCatalogService
+from main.models import Image, Position, FotoSliderBase, TimeSlideBase, Section, CatalogService
 from adminsortable.admin import NonSortableParentAdmin, SortableStackedInline
+
 
 admin.site.register(FotoSliderBase)
 admin.site.register(TimeSlideBase)
@@ -16,16 +16,17 @@ class PositionAdmin(admin.ModelAdmin):
         return False
 
 
-@admin.register(Service)
-class ServicAdmin(admin.ModelAdmin):
+@admin.register(Section)
+class SectionAdmin(admin.ModelAdmin):
     """Раздел"""
-    list_display = 'name', 'position', 'is_active'
-    list_editable = 'position', 'is_active'
-    list_filter = 'is_active',
-    search_fields = 'name',
+    pass
+#     list_display = 'name', 'position', 'is_active'
+#     list_editable = 'position', 'is_active'
+#     list_filter = 'is_active',
+#     search_fields = 'name',
 
-    # def has_module_permission(self, request):
-    #     return False
+# def has_module_permission(self, request):
+#     return False
 
 
 class ImageInline(SortableStackedInline):
@@ -44,41 +45,30 @@ class ImageInline(SortableStackedInline):
 @admin.register(CatalogService)
 class CatalogServiceAdmin(NonSortableParentAdmin):
     """Услуги"""
-    list_display = 'name', 'marker', 'position', 'service', 'is_active',
-    list_editable = 'marker', 'is_active', 'position', 'service', 'is_active',
-    list_filter = 'marker', 'service', 'is_active',
-    search_fields = 'name',
+    pass
+    # list_display = 'name', 'marker', 'position', 'service', 'is_active',
+    # list_editable = 'marker', 'is_active', 'position', 'service', 'is_active',
+    # list_filter = 'marker', 'service', 'is_active',
+    # search_fields = 'name',
     inlines = [ImageInline]
 
 
 """СЕРЕДИНА"""
 
 
-@admin.register(MidServices)
-class MidServicesAdmin(admin.ModelAdmin):
-    """Раздел середина"""
-    pass
-
-    # def has_module_permission(self, request):
-    #     return False
 
 
-class MidImageInline(SortableStackedInline):
-    """Фотографии середина"""
-    model = MidImage
+
+from django.contrib import admin
+from adminsortable.admin import (SortableAdmin, SortableTabularInline)
+from .models import (Gallery, GalleryImageRelation)
+
+class GalleryImageRelationInlineAdmin(SortableTabularInline):
+    model = GalleryImageRelation
     extra = 1
-    readonly_fields = 'preview',
 
-    def preview(self, obj):
-        return mark_safe(f'<img src="{obj.image.url}" width="80" height="80">')
-
-    preview.short_description = 'Превью Фотографии'
+class GalleryAdmin(NonSortableParentAdmin):
+    inlines = (GalleryImageRelationInlineAdmin,)
 
 
-@admin.register(MidCatalogService)
-class MidCatalogServiceAdmin(NonSortableParentAdmin):
-    """Услуги середина"""
-    pass
-    inlines = [MidImageInline]
-
-
+admin.site.register(Gallery, GalleryAdmin)
--- main/models.py
+++ main/models.py
@@ -3,7 +3,6 @@ from django.db import models
 from adminsortable.models import SortableMixin
 from adminsortable.fields import SortableForeignKey
 
-
 class FotoSliderBase(models.Model):
     """Фотослайдер большой"""
     image = models.ImageField('Фотография для слайдера', upload_to='Base_slider/image/')
@@ -24,9 +23,90 @@ class TimeSlideBase(models.Model):
     class Meta:
         verbose_name = 'Таймер времени паузы для фотослайдера'
         verbose_name_plural = 'Таймер времени паузы для фотослайдеров'
+#
+#
+# """ЛЕВАЯ ПАНЕЛЬ"""
+#
+#
+# class Position(models.Model):
+#     """Номер очереди"""
+#     number = models.PositiveIntegerField(verbose_name='Номер очереди', unique=True)
+#
+#     class Meta:
+#         verbose_name = 'Номер очереди'
+#         verbose_name_plural = 'Номер очереди'
+#
+#     def __str__(self):
+#         return f'{self.number}'
+#
+#
+# class Service(models.Model):
+#     """Раздел"""
+#     name = models.CharField(verbose_name='Раздел', max_length=250, unique=True)
+#
+#     position = models.OneToOneField(Position, verbose_name='Номер очереди', on_delete=models.CASCADE)
+#     is_active = models.BooleanField(verbose_name='Активен', default=True)
+#
+#     class Meta:
+#         verbose_name = 'Раздел'
+#         verbose_name_plural = 'Раздел'
+#         ordering = ['position']
+#
+#     def __str__(self):
+#         return self.name
+#
+#     def save(self, *args):
+#         limits = 10
+#         if Service.objects.count() < limits:
+#             super().save(*args)
+#         else:
+#             raise ValidationError('Слишком много записей в Разделе!')
+#
+#
+# class Image(SortableMixin):
+#     """Фотографии"""
+#     category = SortableForeignKey('CatalogService', on_delete=models.CASCADE)
+#     image = models.ImageField(verbose_name='Фотографии', null=True, blank=True, upload_to='photos/')
+#     project_order = models.PositiveIntegerField(default=0, editable=False, db_index=True)
+#
+#     class Meta:
+#         verbose_name = 'Фотографии'
+#         verbose_name_plural = 'Фотографии'
+#         ordering = ['project_order']
+#
+#     def __str__(self):
+#         return self.category.name
+#
+#
+class CatalogService(models.Model):
+    """Услуги"""
+    MARKER_CHOICES = (
+        ('ХИТ', 'ХИТ',),
+        ('ТОП', 'ТОП',),
+        ('NEW', 'NEW',),
+        ('АКЦИЯ', 'АКЦИЯ'),
+    )
+    name = models.CharField(verbose_name='Название', max_length=200, unique=True)
+    marker = models.CharField(verbose_name='Маркер', max_length=200, choices=MARKER_CHOICES, blank=True, null=True)
+    # service = models.ForeignKey('Service', verbose_name='Раздел', on_delete=models.CASCADE)
+    description = models.TextField(verbose_name='Описание', null=True, blank=True)
+    price = models.DecimalField(verbose_name='Цена', max_digits=8, decimal_places=2)
+    # position = models.OneToOneField('Position', verbose_name='Номер очереди', on_delete=models.CASCADE)
+    is_active = models.BooleanField(verbose_name='Активен', default=True)
+
+    class Meta:
+        verbose_name = 'Услуги'
+        verbose_name_plural = 'Услуги'
+        # ordering = ['position']
+
+    def __str__(self):
+        return self.name
 
 
-"""ЛЕВАЯ ПАНЕЛЬ"""
+""""""
+from django.db import models
+from adminsortable.models import SortableMixin
+from adminsortable.fields import SortableForeignKey
 
 
 class Position(models.Model):
@@ -41,26 +121,19 @@ class Position(models.Model):
         return f'{self.number}'
 
 
-class Service(models.Model):
+class Section(models.Model):
     """Раздел"""
     name = models.CharField(verbose_name='Раздел', max_length=250, unique=True)
     position = models.OneToOneField(Position, verbose_name='Номер очереди', on_delete=models.CASCADE)
     is_active = models.BooleanField(verbose_name='Активен', default=True)
 
     class Meta:
-        verbose_name = 'Раздел'
-        verbose_name_plural = 'Раздел'
+        verbose_name = '- Раздел'
+        verbose_name_plural = '- Раздел'
         ordering = ['position']
 
     def __str__(self):
         return self.name
-
-    def save(self, *args):
-        limits = 10
-        if Service.objects.count() < limits:
-            super().save(*args)
-        else:
-            raise ValidationError('Слишком много записей в Разделе!')
 
 
 class Image(SortableMixin):
@@ -75,93 +148,28 @@ class Image(SortableMixin):
         ordering = ['project_order']
 
     def __str__(self):
-        return ''
+        return self.category.name
 
-
-class CatalogService(models.Model):
-    """Услуги"""
-    MARKER_CHOICES = (
-        ('ХИТ', 'ХИТ',),
-        ('ТОП', 'ТОП',),
-        ('NEW', 'NEW',),
-        ('АКЦИЯ', 'АКЦИЯ'),
-    )
-    name = models.CharField(verbose_name='Название', max_length=200, unique=True)
-    marker = models.CharField(verbose_name='Маркер', max_length=200, choices=MARKER_CHOICES, blank=True, null=True)
-    service = models.ForeignKey('Service', verbose_name='Раздел', on_delete=models.CASCADE)
-    description = models.TextField(verbose_name='Описание', null=True, blank=True)
-    price = models.DecimalField(verbose_name='Цена', max_digits=8, decimal_places=2)
-    position = models.OneToOneField('Position', verbose_name='Номер очереди', on_delete=models.CASCADE)
-    is_active = models.BooleanField(verbose_name='Активен', default=True)
+class Gallery(models.Model):
+    section = models.ForeignKey('Section', verbose_name='Раздел', on_delete=models.CASCADE)
 
     class Meta:
-        verbose_name = 'Услуги'
-        verbose_name_plural = 'Услуги'
-        ordering = ['position']
+        verbose_name = '- Панель'
+        verbose_name_plural = '- панель'
 
     def __str__(self):
-        return self.name
+        return self.section.name
 
 
-"""СЕРЕДИНА"""
-
-
-class MidServices(models.Model):
-    """Раздел середина"""
-    title = models.CharField(verbose_name='Раздел', max_length=250, unique=True)
-    info = models.CharField(verbose_name='Информация', max_length=250, unique=True)
-    tips = models.TextField(verbose_name='Читать советы')
-    position = models.OneToOneField(Position, verbose_name='Номер очереди', on_delete=models.CASCADE)
-    is_active = models.BooleanField(verbose_name='Активен', default=True)
+class GalleryImageRelation(SortableMixin):
+    gallery = models.ForeignKey('Gallery', verbose_name="Gallery", on_delete=models.CASCADE)
+    image = SortableForeignKey('CatalogService', verbose_name="Услуги", on_delete=models.CASCADE)
+    image_order = models.PositiveIntegerField(default=0, editable=False, db_index=True)
 
     class Meta:
-        verbose_name = 'Раздел середина'
-        verbose_name_plural = 'Раздел середина'
-        ordering = ['position']
-
-    def __str__(self):
-        return self.title
-
-    def save(self, *args):
-        limits = 10
-        if Service.objects.count() < limits:
-            super().save(*args)
-        else:
-            raise ValidationError('Слишком много записей в Разделе!')
-
-
-class MidImage(SortableMixin):
-    """Фотографии середина"""
-    category = SortableForeignKey('MidCatalogService', on_delete=models.CASCADE)
-    image = models.ImageField(verbose_name='Фотографии', null=True, blank=True, upload_to='photos/')
-    project_order = models.PositiveIntegerField(default=0, editable=False, db_index=True)
-
-    class Meta:
-        verbose_name = 'Фотографии середина'
-        verbose_name_plural = 'Фотографии середина'
-        ordering = ['project_order']
+        verbose_name = '- Услуги'
+        verbose_name_plural = '- Услуги'
+        ordering = ['image_order']
 
     def __str__(self):
         return ''
-
-
-class MidCatalogService(models.Model):
-    """Услуги середина"""
-    name = models.CharField(verbose_name='Название', max_length=200, unique=True)
-    service = models.ForeignKey('MidServices', verbose_name='Раздел середина', on_delete=models.CASCADE)
-    description = models.TextField(verbose_name='Описание', null=True, blank=True)
-    price = models.DecimalField(verbose_name='Цена', max_digits=8, decimal_places=2)
-    position = models.OneToOneField('Position', verbose_name='Номер очереди', on_delete=models.CASCADE)
-    is_active = models.BooleanField(verbose_name='Активен', default=True)
-
-    class Meta:
-        verbose_name = 'Услуги середина'
-        verbose_name_plural = 'Услуги середина'
-        ordering = ['position']
-
-    def __str__(self):
-        return self.name
-
-
-
-
