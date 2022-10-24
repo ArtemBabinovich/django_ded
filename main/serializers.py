4c6a760078474d69b6ecd19992427d5b12bc2ebd
artem <zalivonka02@gmail.com>
2022-10-24 12:11:25 +0300

    update serializers.py

--- main/serializers.py
+++ main/serializers.py
@@ -7,25 +7,25 @@ class FotoSliderBaseSerializer(serializers.ModelSerializer):
     """Главный фотослайдер"""
     class Meta:
         model = FotoSliderBase
-        fields = ['image', 'text', 'color', 'text_2', 'color_2']
+        fields = ['id', 'image', 'text', 'color', 'text_2', 'color_2']
 
 
 class TimeSlideBaseSerializer(serializers.ModelSerializer):
     """Таймер для главного фотослайдера"""
     class Meta:
         model = TimeSlideBase
-        fields = ['time_pause', ]
+        fields = ['id', 'time_pause', ]
 
 
 class MiniFotoSliderSerializer(serializers.ModelSerializer):
     """Мини-фотослайдер"""
     class Meta:
         model = MiniFotoSlider
-        fields = ['image', 'text_1', 'color_1', 'text_2', 'color_2', 'text_3', 'color_3']
+        fields = ['id', 'image', 'text_1', 'color_1', 'text_2', 'color_2', 'text_3', 'color_3']
 
 
 class TimeForMiniSliderSerializer(serializers.ModelSerializer):
     """Таймер для мини-фотослайдера"""
     class Meta:
         model = TimeForMiniSlider
-        fields = ['time_pause', ]
+        fields = ['id', 'time_pause', ]
