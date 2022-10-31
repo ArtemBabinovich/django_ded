f8fad3fa7730d0dbbf36810997482d824cb7b799
sinelnikov7 <sinelnikovby@gmail.com>
2022-10-31 21:43:02 +0300

    Добавлена и зарегистрирована в админке модель Phone

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,5 @@
+from colorfield.fields import ColorField
+from django.core.validators import RegexValidator
 from django.db import models
 
 
@@ -13,3 +15,21 @@ class SocialNetworks(models.Model):
 
     def __str__(self):
         return self.name
+
+class Phone(models.Model):
+    "Телефонный номер"
+    code = models.CharField('Код страны', max_length=2, default='+7',
+                            validators=[RegexValidator(regex=r'\+7', message='Код должен быть равен "+7"')])
+    number_operator = models.CharField('Код оператора', max_length=3,
+                                          validators=[RegexValidator(regex=r'^\d{3}$', message='Код оператора должен состоять из 3 цифр')])
+    number = models.CharField('Номер телефона', max_length=9,
+                                          validators=[RegexValidator(regex=r'^\d{3}[-]?\d{2}[-]?\d{2}$',
+                                                                     message='Код оператора должен состоять из 7 цифр')])
+    color_number = ColorField(default='#FF0000', verbose_name='Цвет номера')
+
+    class Meta:
+        verbose_name = 'Телефонный номер'
+        verbose_name_plural = 'Телефонные номера'
+
+    def __str__(self):
+        return (self.code + " " + str(self.number_operator) + " " + str(self.number))
