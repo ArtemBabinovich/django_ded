7885a3f7d0fa5f0fa94c35b5e0772017f9f1f843
artem <zalivonka02@gmail.com>
2022-11-11 18:17:15 +0300

    add index.html

--- static/js/slider.js
+++ static/js/slider.js
@@ -1,77 +1,72 @@
-const sliderWrapper = document.querySelector('.main__slider-wrapper');
-const sliderNext = document.querySelector('.slider__action-next');
-const sliderPrev = document.querySelector('.slider__action-prev');
-const mainSlider = document.querySelector('.main__slider')
-const main = document.querySelector('main')
+const mainSwiperWrapper = document.querySelector('.swiper-wrapper');
 
-function get__sliderItem(url) {
+
+function getSwiperItem(url) {
     fetch(url)
         .then(resp => resp.json())
-        .then(sliderItem => {
-            for (let i of sliderItem){
-                sliderWrapper.innerHTML += `<div class="slider__item">
-                                                <div class="item__image">
-                                                    <img src='${i.image}' alt="">
-                                                </div>
-                                                <div class="container">
-                                                    <a href="#" class="slider__item-link" style="color: ${i.color}">
-                                                        <p style="color: ${i.color ? i.color:''}">${i.text}</p>
-                                                        ${i.text_2 ? `<span style="color: ${i.color_2}">${i.text_2}</span>`:''}
-                                                    </a>
-                                                </div>
+        .then(item => {
+            for (let i of item) {
+                mainSwiperWrapper.innerHTML += `<div class="main__slider-slide swiper-slide">
+                                                    <div class="main__slider-slide-image">
+                                                        <img class="main__slider-image" src='${i.image}' alt="">
+                                                    </div>
+                                                    <div class="container">
+                                                        <a href="#" class="main__slider-slide-link">
+                                                            <p style="color: ${i.color}">
+                                                                ${i.text}
+                                                            </p>
+                                                            <p style="color: ${i.color_2 ? i.color_2 : i.color};">
+                                                                ${i.text_2 ? i.text_2 : ''}
+                                                            </p>
+                                                        </a>
+                                                    </div>
                                             </div>`
             }
-            const mainSliderItem = document.querySelectorAll('.slider__item');
-            mainSliderItem.forEach(item => {
-                item.style.minWidth = `${sliderWrapper.clientWidth}px`
+            const swiper = new Swiper ('.swiper', {
+                pagination: {
+                    el:'.swiper-pagination',
+                    type: 'bullets',
+                    clickable:true,
+                },
+                loop: true,
+                simulateTouch: false,
+                navigation: {
+                    nextEl: '.slider__action-next',
+                    prevEl: '.slider__action-prev',
+                },
+                slidesPevView: 1,
+                autoplay: {
+                    delay: 2000,
+                    disableOnInteraction: false,
+                    pauseOnMouseEnter:true,
+                }
+            });
+            const mainSliderWrapper = document.querySelector('.main__slider-wrapper');
+            const mainSliderImage = document.querySelectorAll('.main__slider-image');
+            const zoomWindow = document.querySelector('.main__slider-zoomWindow');
+            mainSliderWrapper.addEventListener('mousemove', (e) => {
+                let src = '';
+                zoomWindow.style.display = 'block'
+                zoomWindow.style.top = `${e.offsetY - 40}px`
+                zoomWindow.style.left = `${e.offsetX + 30}px`
+                // console.log(e.offsetY, e.offsetX)
+                mainSliderImage.forEach(slidesItem => {
+                    if (e.target === slidesItem) {
+                        src = slidesItem.getAttribute('src')
+                        // console.log(src)
+                    }
+                })
+                zoomWindow.style.background = `url(${src})`;
+                zoomWindow.style.backgroundRepeat = 'no-repeat'
+                zoomWindow.style.backgroundSize = `${window.innerWidth}px`;
+                zoomWindow.style.backgroundPositionX = `${-e.offsetX}px`
+                zoomWindow.style.backgroundPositionY = `${-e.offsetY}px`
+            })
+            mainSliderWrapper.addEventListener('mouseout', () => {
+                zoomWindow.style.display = 'none'
             })
         })
 }
-get__sliderItem('http://developer.itec.by/api/foto_slider_base/');
-let count = 0;
-sliderNext.addEventListener('click', () => {
-    if (count === (sliderWrapper.clientWidth * 18) - sliderWrapper.clientWidth){
-
-    }else {
-        count += sliderWrapper.clientWidth;
-        sliderWrapper.style.transform = `translateX(${-(count)}px)`
-    }
-})
-sliderPrev.addEventListener('click', () => {
-    if (count === 0){
-
-    }else {
-        count -= sliderWrapper.clientWidth;
-        sliderWrapper.style.transform = `translateX(${-count}px)`
-    }
-})
-let sliderInterval = setInterval(() => {
-    if (count === (sliderWrapper.clientWidth * 18) - sliderWrapper.clientWidth){
-        count = -sliderWrapper.clientWidth;
-    }else {
-        count += sliderWrapper.clientWidth;
-        sliderWrapper.style.transform = `translateX(${-(count)}px)`
-    }
-},4000)
 
 
-mainSlider.addEventListener('mousemove', (e) => {
-    if (e.currentTarget){
-        console.log(e.currentTarget)
-        clearInterval(sliderInterval)
-    }
-})
-mainSlider.addEventListener('mouseout', () => {
-    console.log(11)
-    sliderInterval = setInterval(() => {
-        if (count === (sliderWrapper.clientWidth * 18) - sliderWrapper.clientWidth){
-            count = -sliderWrapper.clientWidth;
-        }else {
-            count += sliderWrapper.clientWidth;
-            sliderWrapper.style.transform = `translateX(${-(count)}px)`
-        }
-    },4000);
-})
-
-let a = new Audio('./audio/logoMusic.mp3')
-console.log(a)
+getSwiperItem('https://developer.itec.by/api/foto_slider_base/')
