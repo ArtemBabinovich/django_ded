24be8806014fd7c2f2fadd4d937829cd312b212e
spilbergg <spilbergg@yandex.ru>
2022-11-13 21:59:41 +0300

    add views for big_slider and small_slider

--- services/views.py
+++ services/views.py
@@ -1,13 +1,15 @@
+from django.db.models import Prefetch
 from rest_framework import viewsets
 from rest_framework.response import Response
 
-from services.models import ServicesCatalog
-from services.serializers import ServicesCatalogSerializer
+from services.models import ServicesCatalog, Services
+from services.serializers import ServicesCatalogSerializer, BigSliderSerializer, SmallSliderSerializer
 
 
 class ServicesCatalogViewSet(viewsets.ReadOnlyModelViewSet):
-    """Отоброжение отфильтрованных данных по полю 'Активная'"""
-    queryset = ServicesCatalog.objects.filter(is_active=True).filter(services__is_active=True)
+    """Представление отфильтрованных данных по полю 'Активная'"""
+    queryset = ServicesCatalog.objects.filter(is_active=True) \
+        .prefetch_related(Prefetch('services', queryset=Services.objects.filter(is_active=True)))
     serializer_class = ServicesCatalogSerializer
 
     def list(self, request, *args, **kwargs):
@@ -18,3 +20,19 @@ class ServicesCatalogViewSet(viewsets.ReadOnlyModelViewSet):
             return self.get_paginated_response(serializer.data)
         serializer = self.get_serializer(queryset, many=True)
         return Response({'data': serializer.data})
+
+
+class BigSliderViewSet(viewsets.ReadOnlyModelViewSet):
+    """Представление большого слайдера"""
+    queryset = ServicesCatalog.objects \
+        .filter(is_active=True) \
+        .prefetch_related(Prefetch('services', queryset=Services.objects.filter(is_active=True)))
+    serializer_class = BigSliderSerializer
+
+
+class SmallSliderViewSet(viewsets.ReadOnlyModelViewSet):
+    """Представление маленького слайдера"""
+    queryset = Services.objects \
+        .filter(is_active=True, service_catalog__is_active=True) \
+        .select_related('service_catalog', 'position_service')
+    serializer_class = SmallSliderSerializer
