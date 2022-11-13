cb749971d39835deb7effa03267291584ca87ec8
spilbergg <spilbergg@yandex.ru>
2022-11-13 21:54:56 +0300

    add checkbox for list display

--- services/admin.py
+++ services/admin.py
@@ -6,18 +6,22 @@ from services.models import Services, ServicesCatalog, ServicesCatalogPosition, 
 @admin.register(ServicesCatalog)
 class ServicesCatalogAdmin(admin.ModelAdmin):
     """Создание РАЗДЕЛА УСЛУГ"""
-    fields = ('title', 'additional_title', 'image_for_big_slider', 'is_active', 'url', 'position')
+    fields = ('title', 'additional_title', 'color_title', 'image_for_big_slider', 'is_active', 'url', 'position',)
     list_display = ('title', 'additional_title', 'is_active', 'position')
     list_filter = ['title', 'additional_title', 'position', 'is_active']
+    list_display_links = ['title', 'position']
+    list_editable = ['is_active']
 
 
 @admin.register(Services)
 class ServicesAdmin(admin.ModelAdmin):
     """Создание УСЛУГИ"""
-    fields = ('service_title', 'marker', 'additional_title', 'image_for_mini_slider', 'bottom_description',
-              'service_catalog', 'is_active', 'position_service')
+    fields = ('service_title', 'color_service_title', 'marker', 'additional_title', 'color_additional_title',
+              'image_for_mini_slider', 'bottom_description', 'color_bottom_description', 'service_catalog', 'is_active',
+              'position_service')
     list_display = ('service_title', 'marker', 'service_catalog', 'is_active', 'position_service')
     list_filter = ['position_service', 'service_title', 'marker']
+    list_editable = ['marker', 'is_active', 'service_catalog']
 
 
 @admin.register(ServicesCatalogPosition)
