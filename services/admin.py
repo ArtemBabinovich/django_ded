cd2a9c979648830312c8e7b2cb04f0399038621c
spilbergg <spilbergg@yandex.ru>
2022-11-18 22:03:27 +0300

    fix admin

--- services/admin.py
+++ services/admin.py
@@ -6,19 +6,20 @@ from services.models import Services, ServicesCatalog, ServicesCatalogPosition, 
 @admin.register(ServicesCatalog)
 class ServicesCatalogAdmin(admin.ModelAdmin):
     """Создание РАЗДЕЛА УСЛУГ"""
-    fields = ('title', 'additional_title', 'color_title', 'image_for_big_slider', 'is_active', 'url', 'position',)
+    fields = ('title', 'additional_title', 'url', 'additional_title_2', 'color_title',
+              'image_for_big_slider', 'is_active', 'timer', 'position')
     list_display = ('title', 'additional_title', 'is_active', 'position')
     list_filter = ['title', 'additional_title', 'position', 'is_active']
     list_display_links = ['title', 'position']
     list_editable = ['is_active']
+    prepopulated_fields = {'url': ('title', 'additional_title')}
 
 
 @admin.register(Services)
 class ServicesAdmin(admin.ModelAdmin):
     """Создание УСЛУГИ"""
-    fields = ('service_title', 'color_service_title', 'marker', 'additional_title', 'color_additional_title',
-              'image_for_mini_slider', 'bottom_description', 'color_bottom_description', 'service_catalog', 'is_active',
-              'position_service')
+    fields = ('service_title', 'color_service_title', 'marker', 'image_for_mini_slider', 'bottom_description',
+              'color_bottom_description', 'service_catalog', 'is_active', 'timer', 'position_service')
     list_display = ('service_title', 'marker', 'service_catalog', 'is_active', 'position_service')
     list_filter = ['position_service', 'service_title', 'marker']
     list_editable = ['marker', 'is_active', 'service_catalog']
