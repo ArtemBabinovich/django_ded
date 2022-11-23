efff8a3fc31798a4f8cd373968b6b923cd50b9b1
spilbergg <spilbergg@yandex.ru>
2022-11-23 18:26:11 +0300

    some fix, i dont remeber

--- main/viewsAPI.py
+++ main/viewsAPI.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
-from main.models import TimeSlideBase, TimeForMiniSlider
-from main.serializers import TimeSlideBaseSerializer, TimeForMiniSliderSerializer
+from main.models import TimeSlideBase
+from main.serializers import TimeSlideBaseSerializer
 
 
 class TimeSlideBaseViewSet(viewsets.ReadOnlyModelViewSet):
@@ -9,7 +9,7 @@ class TimeSlideBaseViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = TimeSlideBaseSerializer
 
 
-class TimeForMiniSliderViewSet(viewsets.ReadOnlyModelViewSet):
-    """API Таймер для мини-фотослайдеров"""
-    queryset = TimeForMiniSlider.objects.all()
-    serializer_class = TimeForMiniSliderSerializer
+# class TimeForMiniSliderViewSet(viewsets.ReadOnlyModelViewSet):
+#     """API Таймер для мини-фотослайдеров"""
+#     queryset = TimeForMiniSlider.objects.all()
+#     serializer_class = TimeForMiniSliderSerializer
