f803327509498a50ab722825baef1512e59337c2
spilbergg <spilbergg@yandex.ru>
2022-11-26 20:03:04 +0300

    fix

--- services/admin.py
+++ services/admin.py
@@ -6,7 +6,7 @@ from services.models import Services, ServicesCatalog, ServicesCatalogPosition, 
 @admin.register(ServicesCatalog)
 class ServicesCatalogAdmin(admin.ModelAdmin):
     """Создание РАЗДЕЛА УСЛУГ"""
-    fields = ('title', 'additional_title', 'url', 'additional_title_2', 'color_title',
+    fields = ('title', 'color_title', 'url', 'additional_title', 'additional_title_2',
               'image_for_big_slider', 'is_active', 'time_pause_for_mini_slider', 'position')
     list_display = ('title', 'additional_title', 'is_active', 'position')
     list_filter = ['title', 'additional_title', 'position', 'is_active']
@@ -19,7 +19,8 @@ class ServicesCatalogAdmin(admin.ModelAdmin):
 class ServicesAdmin(admin.ModelAdmin):
     """Создание УСЛУГИ"""
     fields = ('service_title', 'color_service_title', 'marker', 'image_for_mini_slider', 'bottom_description',
-              'color_bottom_description', 'service_catalog', 'is_active', 'position_service')
+              'color_bottom_description', 'bottom_description_2', 'color_bottom_description_2', 'service_catalog',
+              'is_active', 'position_service', )
     list_display = ('service_title', 'marker', 'service_catalog', 'is_active', 'position_service')
     list_filter = ['position_service', 'service_title', 'marker']
     list_editable = ['marker', 'is_active', 'service_catalog']
