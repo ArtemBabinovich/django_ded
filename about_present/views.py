7e200250959c7573d1594561cfef20abd262f862
spilbergg <spilbergg@yandex.ru>
2022-12-07 00:43:19 +0300

    add task after inviting request.data

--- about_present/views.py
+++ about_present/views.py
@@ -3,29 +3,30 @@ from rest_framework import viewsets, mixins
 from rest_framework.response import Response
 
 from .models import AboutPresent, Recipient, Reason, Present, RemindForDays
-from .serializers import AboutPresentSerializer, RecipientSerializer, ReasonSerializer,\
+from .serializers import AboutPresentSerializer, RecipientSerializer, ReasonSerializer, \
     PresentSerializer, RemindForDaysSerializer
+from .tasks import send
 
 
-class  RecipientViewSets(mixins.ListModelMixin, viewsets.GenericViewSet):
+class RecipientViewSets(mixins.ListModelMixin, viewsets.GenericViewSet):
     """API  всех  получателей подарка"""
     queryset = Recipient.objects.all()
     serializer_class = RecipientSerializer
 
 
-class  ReasonViewSets(mixins.ListModelMixin, viewsets.GenericViewSet):
+class ReasonViewSets(mixins.ListModelMixin, viewsets.GenericViewSet):
     """API  получения всех  событий для  подарка"""
     queryset = Reason.objects.all()
     serializer_class = ReasonSerializer
 
 
-class  PresentViewSets(mixins.ListModelMixin, viewsets.GenericViewSet):
+class PresentViewSets(mixins.ListModelMixin, viewsets.GenericViewSet):
     """API для получения типов подарка"""
     queryset = Present.objects.all()
     serializer_class = PresentSerializer
 
 
-class  RemindForDaysViewSets(mixins.ListModelMixin, viewsets.GenericViewSet):
+class RemindForDaysViewSets(mixins.ListModelMixin, viewsets.GenericViewSet):
     """API для получения всех дат напоминания"""
     queryset = RemindForDays.objects.all()
     serializer_class = RemindForDaysSerializer
@@ -34,11 +35,14 @@ class  RemindForDaysViewSets(mixins.ListModelMixin, viewsets.GenericViewSet):
 class AboutPresentAdd(viewsets.ViewSet):
     """API для оформления подарка"""
     queryset = AboutPresent.objects.all()
+
     @swagger_auto_schema(request_body=AboutPresentSerializer)
     def create(self, request):
         serializer = AboutPresentSerializer(data=request.data)
         if serializer.is_valid():
             response = serializer.save(validated_data=serializer.validated_data)
+            # отправка письма на email при заказе
+            send.delay(response.email, response.name, response.recipient)
             return Response(AboutPresentSerializer(response).data)
         else:
             return Response(serializer.errors)
