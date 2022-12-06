27b1b4a5cc0efe7acb63f1ff444ba68dcc922e6e
spilbergg <spilbergg@yandex.ru>
2022-12-07 00:41:12 +0300

    fixed serializer data for dates field

--- about_present/serializers.py
+++ about_present/serializers.py
@@ -1,5 +1,4 @@
 from django.db import transaction
-
 from rest_framework import serializers
 
 from .models import Recipient, Reason, Present, Date, \
@@ -8,6 +7,7 @@ from .models import Recipient, Reason, Present, Date, \
 
 class RecipientSerializer(serializers.ModelSerializer):
     """Сериализатор для получателя подарка"""
+
     class Meta:
         model = Recipient
         fields = '__all__'
@@ -15,6 +15,7 @@ class RecipientSerializer(serializers.ModelSerializer):
 
 class ReasonSerializer(serializers.ModelSerializer):
     """Сериализатор для причины подарка"""
+
     class Meta:
         model = Reason
         fields = '__all__'
@@ -22,6 +23,7 @@ class ReasonSerializer(serializers.ModelSerializer):
 
 class PresentSerializer(serializers.ModelSerializer):
     """Сериализатор типа подарка"""
+
     class Meta:
         model = Present
         fields = '__all__'
@@ -29,7 +31,7 @@ class PresentSerializer(serializers.ModelSerializer):
 
 class DateSerializer(serializers.ModelSerializer):
     """Сериализатор для даты события"""
-    date = serializers.DateField(required=True)
+
     class Meta:
         model = Date
         fields = ['date']
@@ -37,6 +39,7 @@ class DateSerializer(serializers.ModelSerializer):
 
 class RemindForDaysSerializer(serializers.ModelSerializer):
     """Сериализатор для даты напоминания"""
+
     class Meta:
         model = RemindForDays
         fields = '__all__'
@@ -44,17 +47,19 @@ class RemindForDaysSerializer(serializers.ModelSerializer):
 
 class AboutPresentSerializer(serializers.ModelSerializer):
     """Сериализатор для оформления подарка"""
-    dates = DateSerializer(many=True)
+    about_present = DateSerializer(many=True)
     recipient = serializers.PrimaryKeyRelatedField(queryset=Recipient.objects.all())
     reason = serializers.PrimaryKeyRelatedField(queryset=Reason.objects.all())
     present = serializers.PrimaryKeyRelatedField(queryset=Present.objects.all())
     remind_for_days = serializers.PrimaryKeyRelatedField(queryset=RemindForDays.objects.all())
     remind_every_years = serializers.BooleanField()
+
     class Meta:
         model = AboutPresent
-        fields = ['id', 'name', 'email', 'phone', 'dates', 'recipient',
+        fields = ['id', 'name', 'email', 'phone', 'about_present', 'recipient',
                   'reason', 'present', 'remind_for_days', 'remind_every_years']
 
+    # def validated_date
     def validate_dates(self, value):
 
         if len(value) < 1:
@@ -65,14 +70,14 @@ class AboutPresentSerializer(serializers.ModelSerializer):
     def save(self, validated_data):
 
         with transaction.atomic():
-            present = AboutPresent(name=validated_data['name'], email=validated_data['email'],
-                                   phone=validated_data['phone'], recipient=validated_data['recipient'],
-                                   reason=validated_data['reason'], present=validated_data['present'],
-                                   remind_for_days=validated_data['remind_for_days'],
-                                   remind_every_years=validated_data['remind_every_years'])
-            present.save()
-            for value in validated_data['dates']:
-                date = Date(date=value.get("date"))
-                date.save()
-                present.dates.add(date.id)
-        return present
+            presents = AboutPresent(name=validated_data['name'], email=validated_data['email'],
+                                    phone=validated_data['phone'], recipient=validated_data['recipient'],
+                                    reason=validated_data['reason'], present=validated_data['present'],
+                                    remind_for_days=validated_data['remind_for_days'],
+                                    remind_every_years=validated_data['remind_every_years'],
+                                    )
+            presents.save()
+            # создание дат календаря
+            for value in validated_data['about_present']:
+                Date.objects.create(date=value.get("date"), presents_id=presents.id)
+        return presents
