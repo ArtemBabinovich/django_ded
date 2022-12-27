98e3997e40ab5853a45bebceb21a8fdcd6a911cd
sinelnikov7 <sinelnikovby@gmail.com>
2022-12-27 15:05:39 +0300

    changed_models

--- modules/APIviews.py
+++ modules/APIviews.py
@@ -1,46 +1,46 @@
-from rest_framework import viewsets, mixins
-
-from .models import FotoForBanner, Calendar, Timer, Banner, Module
-from .serializers import FotoForBannerSerializer, CalendarSerializer, TimerSerializer,\
-    BannerSerializer, ModuleSerializer
-
-
-class FotoForBannerViewSet(mixins.ListModelMixin,
-                                mixins.RetrieveModelMixin,
-                                viewsets.GenericViewSet):
-    """API фото и текста для слайдера баннера"""
-    queryset = FotoForBanner.objects.all()
-    serializer_class = FotoForBannerSerializer
-
-
-class CalendarViewSet(mixins.ListModelMixin,
-                           mixins.RetrieveModelMixin,
-                           viewsets.GenericViewSet):
-    """API каллендаря"""
-    queryset = Calendar.objects.all()
-    serializer_class = CalendarSerializer
-
-
-class TimerViewSet(mixins.ListModelMixin,
-                           mixins.RetrieveModelMixin,
-                           viewsets.GenericViewSet):
-    """API таймера"""
-    queryset = Timer.objects.all()
-    serializer_class = TimerSerializer
-
-
-class BannerViewSet(mixins.ListModelMixin,
-                           mixins.RetrieveModelMixin,
-                           viewsets.GenericViewSet):
-    """API баннера"""
-    queryset = Banner.objects.all()
-    serializer_class = BannerSerializer
-
-
-class ModuleViewSet(mixins.ListModelMixin,
-                           mixins.RetrieveModelMixin,
-                           viewsets.GenericViewSet):
-    """API модуля"""
-    queryset = Module.objects.all()
-    serializer_class = ModuleSerializer
-# Create your views here.
+# from rest_framework import viewsets, mixins
+#
+# from .models import FotoForBanner, Calendar, Timer, Banner, Module
+# from .serializers import FotoForBannerSerializer, CalendarSerializer, TimerSerializer,\
+#     BannerSerializer, ModuleSerializer
+#
+#
+# class FotoForBannerViewSet(mixins.ListModelMixin,
+#                                 mixins.RetrieveModelMixin,
+#                                 viewsets.GenericViewSet):
+#     """API фото и текста для слайдера баннера"""
+#     queryset = FotoForBanner.objects.all()
+#     serializer_class = FotoForBannerSerializer
+#
+#
+# class CalendarViewSet(mixins.ListModelMixin,
+#                            mixins.RetrieveModelMixin,
+#                            viewsets.GenericViewSet):
+#     """API каллендаря"""
+#     queryset = Calendar.objects.all()
+#     serializer_class = CalendarSerializer
+#
+#
+# class TimerViewSet(mixins.ListModelMixin,
+#                            mixins.RetrieveModelMixin,
+#                            viewsets.GenericViewSet):
+#     """API таймера"""
+#     queryset = Timer.objects.all()
+#     serializer_class = TimerSerializer
+#
+#
+# class BannerViewSet(mixins.ListModelMixin,
+#                            mixins.RetrieveModelMixin,
+#                            viewsets.GenericViewSet):
+#     """API баннера"""
+#     queryset = Banner.objects.all()
+#     serializer_class = BannerSerializer
+#
+#
+# class ModuleViewSet(mixins.ListModelMixin,
+#                            mixins.RetrieveModelMixin,
+#                            viewsets.GenericViewSet):
+#     """API модуля"""
+#     queryset = Module.objects.all()
+#     serializer_class = ModuleSerializer
+# # Create your views here.
--- modules/models.py
+++ modules/models.py
@@ -11,6 +11,131 @@ def validate_current_century(value):
     if value.year > 2050:
         raise ValidationError('Год не может превышать значение 2050')
 
+class Banner1(models.Model):
+    """Баннер №1"""
+    calendar_title = models.CharField('Заголовок календаря', max_length=100)
+    calendar_date = models.DateField(validators=[validate_current_century], verbose_name='Выбор даты')
+    text_title_1 = models.CharField('Заголовок №1', max_length=100)
+    text_1 = models.TextField('Текст №1')
+    slider = models.ManyToManyField('FotoForBanner', verbose_name='Выбрать фото для слайдера')
+    text_title_2 = models.CharField('Заголовок №2', max_length=100)
+    text_2 = models.TextField('Текст №2')
+    discount_amount = models.IntegerField(verbose_name='Сумма скидки')
+    timer_title = models.CharField('Заголовок таймера', max_length=100)
+    timer = models.DateTimeField('Дата и время окончания таймера')
+    banner_position = models.IntegerField(verbose_name='Порядковый номер баннера')
+
+    class Meta:
+        verbose_name = 'Баннер типа №1'
+        verbose_name_plural = 'Баннера типа №1'
+
+    def __str__(self):
+        return f"Баннер №1, порядковый номер - {self.banner_position}"
+
+
+class Title1ForBanner2(models.Model):
+    """Текст №1 для баннера № 2"""
+    text = models.TextField('Текст №1 для баннера № 2')
+
+    def __str__(self):
+        return self.text
+
+
+class Title2ForBanner2(models.Model):
+    """Текст №1 для баннера № 2"""
+    text = models.TextField('Текст №2 для баннера № 2')
+
+    def __str__(self):
+        return self.text
+
+
+class Banner2(models.Model):
+    """Баннер №2"""
+    calendar_title = models.CharField('Заголовок календаря', max_length=100)
+    calendar_date = models.DateField(validators=[validate_current_century], verbose_name='Выбор даты')
+    title = models.CharField('Заголовок', max_length=100)
+    text = models.TextField('Текст')
+    slider_1 = models.ManyToManyField('FotoForBanner', related_name='slider_1', verbose_name='Выбрать фото для слайдера №1')
+    text_1 = models.ManyToManyField('Title1ForBanner2',verbose_name='Текстовое пространство №1')
+    slider_2 = models.ManyToManyField('FotoForBanner', related_name='slider_2', verbose_name='Выбрать фото для слайдера №2')
+    text_2 = models.ManyToManyField('Title2ForBanner2', verbose_name='Текстовое пространство №2')
+    discount_amount = models.IntegerField(verbose_name='Сумма скидки')
+    timer_title = models.CharField('Заголовок таймера', max_length=100)
+    timer = models.DateTimeField('Дата и время окончания таймера')
+    banner_position = models.IntegerField(verbose_name='Порядковый номер баннера')
+
+    class Meta:
+        verbose_name = 'Баннер типа №2'
+        verbose_name_plural = 'Баннера типа №2'
+
+    def __str__(self):
+        return f"Баннер №2, порядковый номер - {self.banner_position}"
+
+
+class Banner3(models.Model):
+    """Баннер №3"""
+    calendar_title = models.CharField('Заголовок календаря', max_length=100)
+    calendar_date = models.DateField(validators=[validate_current_century], verbose_name='Выбор даты')
+    title = models.CharField('Заголовок', max_length=100)
+    slider = models.ManyToManyField('FotoForBanner', verbose_name='Выбрать фото для слайдера')
+    text_title = models.CharField('Заголовок Текста', max_length=100)
+    text = models.TextField('Текст')
+    timer_title = models.CharField('Заголовок таймера', max_length=100)
+    timer = models.DateTimeField('Дата и время окончания таймера')
+    banner_position = models.IntegerField(verbose_name='Порядковый номер баннера')
+
+    class Meta:
+        verbose_name = 'Баннер типа №3'
+        verbose_name_plural = 'Баннера типа №3'
+
+    def __str__(self):
+        return f"Баннер №3, порядковый номер - {self.banner_position}"
+
+
+class TextForBanner4(models.Model):
+    """Текст №1 для баннера № 2"""
+    text = models.TextField('Текст для баннера № 4')
+
+    def __str__(self):
+        return self.text
+
+
+class Banner4(models.Model):
+    """Баннер №3"""
+    banner_title = models.CharField('Заголовок баннера', max_length=100)
+    title = models.CharField('Заголовок', max_length=100)
+    slider = models.ManyToManyField('FotoForBanner', verbose_name='Выбрать фото для слайдера')
+    text_title = models.CharField('Заголовок перед текстовым пространством', max_length=100)
+    text = models.ManyToManyField('TextForBanner4', verbose_name='Текстовое пространство')
+    banner_position = models.IntegerField(verbose_name='Порядковый номер баннера')
+
+    class Meta:
+        verbose_name = 'Баннер типа №4'
+        verbose_name_plural = 'Баннера типа №4'
+
+    def __str__(self):
+        return f"Баннер №4, порядковый номер - {self.banner_position}"
+
+
+class Banner5(models.Model):
+    """Баннер №5"""
+    banner_title = models.CharField('Заголовок баннера', max_length=100)
+    title = models.CharField('Заголовок', max_length=100)
+    slider = models.ManyToManyField('FotoForBanner', verbose_name='Выбрать фото для слайдера')
+    text_title = models.CharField('Заголовок перед текстовым пространством', max_length=100)
+    text = models.TextField('Текст')
+    discount_amount = models.IntegerField(verbose_name='Сумма скидки')
+    banner_position = models.IntegerField(verbose_name='Порядковый номер баннера')
+
+    class Meta:
+        verbose_name = 'Баннер типа №5'
+        verbose_name_plural = 'Баннера типа №5'
+
+    def __str__(self):
+        return f"Баннер №5, порядковый номер - {self.banner_position}"
+
+
+
 
 class FotoForBanner(models.Model):
     """Добовление фотографии, текста и позиции для слайдера в баннере"""
@@ -28,76 +153,19 @@ class FotoForBanner(models.Model):
     def __str__(self):
         return self.name
 
-
-class Calendar(models.Model):
-    """Выбор даты для каллендаря"""
-    banner_calendar = models.DateField(validators=[validate_current_century])
-
-    class Meta:
-        verbose_name = 'Каллендарь'
-        verbose_name_plural = 'Каллендарь'
-
-    def __str__(self):
-        return str(self.banner_calendar)
-
-
-class Timer(models.Model):
-    """Таймеры для баннеров"""
-    name = models.CharField('Название', max_length=50)
-    text = tinymce_models.HTMLField(verbose_name='Текст', null=True, blank=True)
-    timer_days = models.IntegerField('Таймер - дни', default=0)  # Таймер
-    timer_hours = models.IntegerField('Таймер - часы', default=0,
-                                      validators=[MaxValueValidator(24)])
-    timer_minutes = models.IntegerField('Таймер - минуты', default=0,
-                                        validators=[MaxValueValidator(60)])
-    timer_seconds = models.IntegerField('Таймер - секунды', default=0,
-                                        validators=[MaxValueValidator(60)])
-
-    class Meta:
-        verbose_name = 'Таймер'
-        verbose_name_plural = 'Таймеры'
-
-    def __str__(self):
-        return self.name
-
-
-class Banner(models.Model):
-    """Рекламные Баннера"""
-    slider_type_choice = (
-        ('horizontal', 'Горизонтальный'),
-        ('vertical', 'Вертикальный'),
-    )
-    name = models.CharField('Название баннера', max_length=250)
-    calendar = models.ForeignKey(Calendar, on_delete=models.SET(datetime.today().strftime("%Y-%m-%d")),
-                                 null=True, blank=True, verbose_name='Каллендарь')
-    slider_type = models.CharField('Тип слайдера',max_length=50, choices=slider_type_choice)
-    slider_fotos = models.ManyToManyField('FotoForBanner', verbose_name='Выбрать фото для слайдера')
-    slider_speed = models.IntegerField('Скорость переключения слайдера в сек.', default=2)
-    timer = models.ForeignKey(Timer, verbose_name='Таймер', on_delete=models.SET_NULL, null=True, blank=True)
-    url = models.CharField('Внутренняя ссылка', max_length=50)
-    text = tinymce_models.HTMLField(verbose_name='Текст', null=True, blank=True)
-
-    class Meta:
-        verbose_name = 'Баннер'
-        verbose_name_plural = 'Баннера'
-
-    def __str__(self):
-        return self.name
-
-
-class Module(models.Model):
-    name = models.CharField('Название модуля', max_length=250)
-    slot_1 = models.ForeignKey(Banner, on_delete=models.SET_NULL, null=True, blank=True, related_name='slot_1')
-    slot_2 = models.ForeignKey(Banner, on_delete=models.SET_NULL, null=True, blank=True, related_name='slot_2')
-    slot_3 = models.ForeignKey(Banner, on_delete=models.SET_NULL, null=True, blank=True, related_name='slot_3')
-    slot_4 = models.ForeignKey(Banner, on_delete=models.SET_NULL, null=True, blank=True, related_name='slot_4')
-    slot_5 = models.ForeignKey(Banner, on_delete=models.SET_NULL, null=True, blank=True, related_name='slot_5')
-
-    class Meta:
-        verbose_name = 'Модуль'
-        verbose_name_plural = 'Модули'
-
-    def __str__(self):
-        return self.name
+# class Module(models.Model):
+#     name = models.CharField('Название модуля', max_length=250)
+#     slot_1 = models.ForeignKey(Banner, on_delete=models.SET_NULL, null=True, blank=True, related_name='slot_1')
+#     slot_2 = models.ForeignKey(Banner, on_delete=models.SET_NULL, null=True, blank=True, related_name='slot_2')
+#     slot_3 = models.ForeignKey(Banner, on_delete=models.SET_NULL, null=True, blank=True, related_name='slot_3')
+#     slot_4 = models.ForeignKey(Banner, on_delete=models.SET_NULL, null=True, blank=True, related_name='slot_4')
+#     slot_5 = models.ForeignKey(Banner, on_delete=models.SET_NULL, null=True, blank=True, related_name='slot_5')
+#
+#     class Meta:
+#         verbose_name = 'Модуль'
+#         verbose_name_plural = 'Модули'
+#
+#     def __str__(self):
+#         return self.name
 
 
